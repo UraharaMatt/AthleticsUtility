838c4f1a56d7d5a4ba3316bbab3d4b854f6a14c2
UraharaMatt <uraharamatt@gmail.com>
2022-07-05 17:36:50 +0200

    Creation iMPORT FIREBASE

--- app/src/main/java/com/example/athleticsutility/LoginActivity.kt
+++ app/src/main/java/com/example/athleticsutility/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,4 +16,34 @@ class LoginActivity : AppCompatActivity() {
         startActivity(intent)
         finish()
     }
+    /*
+    fun checkLogin(v: View?) {
+        val email: String = editTextUserName.getText().toString()
+        if (!isValidEmail(email)) {
+            //Set error message for email field
+            editTextUserName.setError(getString(R.string.invalid_email))
+        }
+        val pass: String = passwordEditText.getText().toString()
+        if (!isValidPassword(pass)) {
+            //Set error message for password field
+            passwordEditText.setError(getString(R.string.invalid_password))
+        }
+        if (isValidEmail(email) && isValidPassword(pass)) {
+            // Validation Completed
+        }
+    }
+    // validating email id
+    private fun isValidEmail(email: String): Boolean {
+        val EMAIL_PATTERN = ("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")
+        val pattern = Pattern.compile(EMAIL_PATTERN)
+        val matcher = pattern.matcher(email)
+        return matcher.matches()
+    }
+    // validating password
+    private fun isValidPassword(pass: String?): Boolean {
+        return if (pass != null && pass.length >= 4) {
+            true
+        } else false
+    }*/
 }
--- app/src/main/java/com/example/athleticsutility/RegisterActivity.kt
+++ app/src/main/java/com/example/athleticsutility/RegisterActivity.kt
@@ -4,8 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import java.util.regex.Pattern
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private var TAG = "LoginActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
