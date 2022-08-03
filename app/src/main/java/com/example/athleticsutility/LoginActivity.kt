3d262c4532d816d077ca52c7a9d70808b907d434
UraharaMatt <uraharamatt@gmail.com>
2022-08-03 11:28:06 +0200

    FB Google Button UI resolving inflate problem

--- app/src/main/java/com/example/athleticsutility/LoginActivity.kt
+++ app/src/main/java/com/example/athleticsutility/LoginActivity.kt
@@ -2,32 +2,68 @@ package com.example.athleticsutility
 
 import android.content.ContentValues.TAG
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
-import java.util.regex.Pattern
 
-private lateinit var auth: FirebaseAuth;
+
+private lateinit var auth: FirebaseAuth
+private lateinit var googleSignInClient: GoogleSignInClient
+private const val RC_SIGN_IN = 9001
+private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+
+
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         // Initialize Firebase Auth
         auth = Firebase.auth
+
+        // [START config_signin]
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        // [END config_signin]
+        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RC_SIGN_IN) {
+                // There are no request codes
+                val data: Intent? = result.data
+                val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+                try {
+                    val account = task.getResult(ApiException::class.java)!!
+                    firebaseAuthWithGoogle(account.idToken)
+                } catch (e: ApiException) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
         loginButton.setOnClickListener{
             onLoginClick()
+        }
+        loginGoogle.setOnClickListener {
+            onGoogleLogClick()
         }
         signUpTextView.setOnClickListener {
             signUpRedirect()
@@ -70,6 +106,34 @@ class LoginActivity : AppCompatActivity() {
 
 
     }
+    private fun onGoogleLogClick()
+    {
+        val signInIntent = googleSignInClient.signInIntent
+        resultLauncher.launch(signInIntent)
+
+
+    }
+    // [START auth_with_google]
+    private fun firebaseAuthWithGoogle(idToken: String?) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    //val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    //updateUI(null)
+                }
+            }
+    }
+    // [END auth_with_google]
+
+
+
     private fun onRecoveryPasswordClick() {
         /*val intent = Intent(this, RecoveryPassword::class.java)
         startActivity(intent)
@@ -80,4 +144,5 @@ class LoginActivity : AppCompatActivity() {
         startActivity(intent)
         finish()
     }
+
 }
--- app/src/main/java/com/example/athleticsutility/MainActivity.kt
+++ app/src/main/java/com/example/athleticsutility/MainActivity.kt
@@ -10,8 +10,9 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
+private lateinit var auth: FirebaseAuth
 class MainActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
